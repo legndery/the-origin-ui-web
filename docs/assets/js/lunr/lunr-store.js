1e128baeb6f761296f19573d6af6c973fec759e2
legndery <ar_itra@yahoo.com>
2023-03-19 23:05:17 +0530

    git docs

---
+++ docs/assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Loops, Asynchronous functions and Returning Collective Results in Node.js (with bonus trivia at the end)",
+        "excerpt":"I like to solve stackoverflow questions, even sometimes like to browse the node.js tag for relevant questions. One of the most common question pattern is: Hey, I am looping and doing some HTTP requests or Model queries for each iteration and pushing it to my results variable but It is...","categories": ["Javascript"],
+        "tags": [],
+        "url": "/javascript/loops-asynchronous-functions-and-returning-collective-results-in-node-js/",
+        "teaser": null
+      },{
+        "title": "Implementing Publisher-Subscriber pattern from Scratch and addressing common mistakes",
+        "excerpt":"Publisher Subscriber pattern is very simple design pattern to understand and also with the default NodeJS libraries it is also pretty easy to implement programmatically. Also in this article we will try to implement a Event Emitter from scratch. So what the heck is this pub-sub? I mean its as...","categories": ["Javascript"],
+        "tags": [],
+        "url": "/javascript/implementing-publisher-subscriber-pattern-from-scratch-and-addressing-common-mistakes/",
+        "teaser": null
+      }]
